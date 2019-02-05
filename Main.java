a38e00ba72f40a82e36718b2e4d3445c721fbf42
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-05 15:13:28 -0500

    committing Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        String Name1;
+        String Name2;
+        
+        System.out.println("Please enter your name1" );
+        Name1 = scanner.nextLine();
+        System.out.println("Please enter your name2" );
+       Name2 = scanner.nextLine();
+       
+       if ((Name1.equals("Alice"))||(Name2.equals("Bob")))
+       {
+           System.out.println("Hello there " + Name1 + " and " + Name2);
+        }
+           
+           
+           
+        else
+        {
+             System.out.println("No greetings" );
+       
+       
+        
+        
+        
 
     }
+}
 }
